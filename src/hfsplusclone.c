9232c89019c18a87b566effc4c3ae202604a6de5
Dave Vasilevsky <dave@vasilevsky.ca>
2022-02-10 03:45:59 -0500

    be ok even on big-endian hosts

--- src/hfsplusclone.c
+++ src/hfsplusclone.c
@@ -34,24 +34,6 @@ static struct HFSVolumeHeader hsb;
 static UInt64 partition_size;
 int ret;
 
-static short reverseShort(short s){
-    unsigned char c1, c2;
-
-    c1 = s & 255;
-    c2 = (s >> 8) & 255;
-
-    return (c1 << 8)+c2;
-}
-
-static int reverseInt(int i){
-    unsigned char c1, c2, c3, c4;
-    c1 = i & 255;
-    c2 = (i >> 8) & 255;
-    c3 = (i >> 16)& 255;
-    c4 = (i >> 24)& 255;
-
-    return ((int)c1<<24)+((int)c2<<16)+((int)c3<<8)+c4;
-}
 
 static int IsAllocationBlockUsed(UInt32 thisAllocationBlock, UInt8* allocationFileContents)
 {
@@ -67,12 +49,12 @@ static void print_fork_data(HFSPlusForkData* fork){
 
     HFSPlusExtentDescriptor* exten;
     log_mesg(2, 0, 0, fs_opt.debug, "%s: logicalSize: %#lx\n", __FILE__, fork->logicalSize);
-    log_mesg(2, 0, 0, fs_opt.debug, "%s: clumpSize: %i\n", __FILE__, reverseInt(fork->clumpSize));
-    log_mesg(2, 0, 0, fs_opt.debug, "%s: totalBlocks: %i\n", __FILE__, reverseInt(fork->totalBlocks));
+    log_mesg(2, 0, 0, fs_opt.debug, "%s: clumpSize: %i\n", __FILE__, be32toh(fork->clumpSize));
+    log_mesg(2, 0, 0, fs_opt.debug, "%s: totalBlocks: %i\n", __FILE__, be32toh(fork->totalBlocks));
     for (i = 0; i < 8; i++ ){
         exten = &fork->extents[i];
-        log_mesg(2, 0, 0, fs_opt.debug, "%s: \texten %i startBlock: %i\n", __FILE__, i, reverseInt(exten->startBlock));
-        log_mesg(2, 0, 0, fs_opt.debug, "%s: \texten %i blockCount: %i\n", __FILE__, i, reverseInt(fork->extents[i].blockCount));
+        log_mesg(2, 0, 0, fs_opt.debug, "%s: \texten %i startBlock: %i\n", __FILE__, i, be32toh(exten->startBlock));
+        log_mesg(2, 0, 0, fs_opt.debug, "%s: \texten %i blockCount: %i\n", __FILE__, i, be32toh(fork->extents[i].blockCount));
 
     }
 }
@@ -80,9 +62,9 @@ static void print_fork_data(HFSPlusForkData* fork){
 static UInt64 hfs_embed_offset() {
     int wrapper_block_size;
 
-    wrapper_block_size = reverseInt(hsb.allocationBlockSize);
-    return (UInt64)reverseShort(hsb.firstAllocationBlock) * PART_SECTOR_SIZE +
-        reverseShort(hsb.embedExtent.startBlock) * wrapper_block_size;
+    wrapper_block_size = be32toh(hsb.allocationBlockSize);
+    return (UInt64)be16toh(hsb.firstAllocationBlock) * PART_SECTOR_SIZE +
+        be16toh(hsb.embedExtent.startBlock) * wrapper_block_size;
 }
 
 // Try to use this device as an HFS+ volume embedded in an HFS wrapper
@@ -96,7 +78,7 @@ static void open_wrapped_volume(char *device, short *signature, char *buffer) {
     partition_size = get_partition_size(&ret);
 
     memcpy(&hsb, &sb, sizeof(HFSVolumeHeader)); // HFS header is always smaller
-    wrapper_signature = reverseShort(hsb.embedSignature);
+    wrapper_signature = be16toh(hsb.embedSignature);
     if (wrapper_signature != HFSPlusSignature)
         log_mesg(0, 1, 1, fs_opt.debug, "%s: HFS_Plus volume is really just HFS, can't clone that.\n", __FILE__);
 
@@ -107,21 +89,21 @@ static void open_wrapped_volume(char *device, short *signature, char *buffer) {
     if (read(ret, buffer, sizeof(HFSPlusVolumeHeader)) != sizeof(HFSPlusVolumeHeader))
         log_mesg(0, 1, 1, fs_opt.debug, "%s: read embedded HFSPlusVolumeHeader fail.\n", __FILE__);
     memcpy(&sb, buffer, sizeof(HFSPlusVolumeHeader));
-    *signature = reverseShort(sb.signature);
+    *signature = be16toh(sb.signature);
 
     if (*signature == HFSPlusSignature) {
         log_mesg(0, 0, 0, fs_opt.debug, "%s: HFS_Plus embedded volume found at offset %lu.\n", __FILE__, embed_offset);
 
         // Do some sanity checks
-        hfsp_block_size = reverseInt(sb.blockSize);
-        wrapper_block_size = reverseInt(hsb.allocationBlockSize);
+        hfsp_block_size = be32toh(sb.blockSize);
+        wrapper_block_size = be32toh(hsb.allocationBlockSize);
         if (wrapper_block_size % hfsp_block_size != 0)
             log_mesg(0, 1, 1, fs_opt.debug, "%s: HFS_Plus wrapper block size %u is not a multiple of block size %u.\n", __FILE__, wrapper_block_size, hfsp_block_size);
         if (embed_offset % hfsp_block_size != 0)
             log_mesg(0, 1, 1, fs_opt.debug, "%s: HFS_Plus embedded volume offset %lu is not a multiple of block size %u.\n", __FILE__, embed_offset, hfsp_block_size);
 
-        embed_size = (UInt64)wrapper_block_size * (UInt16)reverseShort(hsb.embedExtent.blockCount);
-        hfsp_size = (UInt64)hfsp_block_size * reverseInt(sb.totalBlocks);
+        embed_size = (UInt64)wrapper_block_size * (UInt16)be16toh(hsb.embedExtent.blockCount);
+        hfsp_size = (UInt64)hfsp_block_size * be32toh(sb.totalBlocks);
         if (embed_size != hfsp_size)
             log_mesg(0, 1, 1, fs_opt.debug, "%s: HFS_Plus embedded volume size %lu doesn't match wrapper embed size %lu.\n", __FILE__, hfsp_size, embed_size);
     }
@@ -146,7 +128,7 @@ static void fs_open(char* device){
 	log_mesg(0, 1, 1, fs_opt.debug, "%s: read HFSPlusVolumeHeader fail\n", __FILE__);
     memcpy(&sb, buffer, sizeof(HFSPlusVolumeHeader));
 
-    HFS_Signature = reverseShort(sb.signature);
+    HFS_Signature = be16toh(sb.signature);
 
     if(HFS_Signature == HFSSignature) {
         open_wrapped_volume(device, &HFS_Signature, buffer);
@@ -157,13 +139,13 @@ static void fs_open(char* device){
             (char)(HFS_Signature >> 8), (char)HFS_Signature);
     }
 
-    HFS_Version = (short)reverseShort(sb.version);
-    HFS_Clean = (reverseInt(sb.attributes)>>8) & 1;
+    HFS_Version = (short)be16toh(sb.version);
+    HFS_Clean = (be32toh(sb.attributes)>>8) & 1;
     log_mesg(3, 0, 0, fs_opt.debug, "%s: Signature=%c%c\n", __FILE__,
         (char)(HFS_Signature >> 8), (char)HFS_Signature);
     log_mesg(3, 0, 0, fs_opt.debug, "%s: Version=%i\n", __FILE__, HFS_Version);
     log_mesg(3, 0, 0, fs_opt.debug, "%s: Attr-Unmounted=%i(1 is clean, 0 is dirty)\n", __FILE__, HFS_Clean);
-    log_mesg(3, 0, 0, fs_opt.debug, "%s: Attr-Inconsistent=%i\n", __FILE__, (reverseInt(sb.attributes)>>11) & 1);
+    log_mesg(3, 0, 0, fs_opt.debug, "%s: Attr-Inconsistent=%i\n", __FILE__, (be32toh(sb.attributes)>>11) & 1);
 
     if(fs_opt.ignore_fschk){
         log_mesg(1, 0, 0, fs_opt.debug, "%s: Ignore filesystem check\n", __FILE__);
@@ -214,14 +196,14 @@ static void read_allocation_file(unsigned long* bitmap, progress_bar *prog, UInt
     UInt32 allocation_block_size;
     UInt64 byte_offset = 0;
 
-    tb = reverseInt(sb.totalBlocks);
-    block_size = reverseInt(sb.blockSize);
+    tb = be32toh(sb.totalBlocks);
+    block_size = be32toh(sb.blockSize);
     byte_offset = block_offset * block_size;
 
     for (allocation_exten = 0; allocation_exten <= 7; allocation_exten++){
-        allocation_start_block = block_size*reverseInt(sb.allocationFile.extents[allocation_exten].startBlock);
+        allocation_start_block = block_size*be32toh(sb.allocationFile.extents[allocation_exten].startBlock);
 
-        allocation_block_size = block_size*reverseInt(sb.allocationFile.extents[allocation_exten].blockCount);
+        allocation_block_size = block_size*be32toh(sb.allocationFile.extents[allocation_exten].blockCount);
         log_mesg(2, 0, 0, 2, "%s: tb = %lu\n", __FILE__, tb);
         log_mesg(2, 0, 0, 2, "%s: extent_block = %lu\n", __FILE__, extent_block);
         log_mesg(2, 0, 0, 2, "%s: allocation_exten = %i\n", __FILE__, allocation_exten);
@@ -259,7 +241,7 @@ static void read_allocation_file(unsigned long* bitmap, progress_bar *prog, UInt
         log_mesg(2, 0, 0, 2, "%s: bfree:%i\n", __FILE__, bfree);
         log_mesg(2, 0, 0, 2, "%s: bused:%i\n", __FILE__, bused);
     }
-    mused = (reverseInt(sb.totalBlocks) - reverseInt(sb.freeBlocks));
+    mused = (be32toh(sb.totalBlocks) - be32toh(sb.freeBlocks));
     if(bused != mused)
         log_mesg(0, 1, 1, fs_opt.debug, "%s: bitmap count error, used:%lu, mbitmap:%lu\n", __FILE__, bused, mused);
 }
@@ -274,7 +256,7 @@ void read_bitmap(char* device, file_system_info fs_info, unsigned long* bitmap, 
     UInt32 block_size = 0, block_offset = 0;
 
     fs_open(device);
-    tb = reverseInt(sb.totalBlocks);
+    tb = be32toh(sb.totalBlocks);
 
     /// init progress
     progress_bar   prog;	/// progress_bar structure defined in progress.h
@@ -284,9 +266,9 @@ void read_bitmap(char* device, file_system_info fs_info, unsigned long* bitmap, 
 
     if (hsb.signature != 0) {
         embed_offset = hfs_embed_offset();
-        block_size = reverseInt(sb.blockSize);
-        block_offset = embed_offset / reverseInt(sb.blockSize);
-        embed_end = embed_offset + (UInt64)reverseInt(hsb.allocationBlockSize) * (UInt16)reverseShort(hsb.allocationBlockCount);
+        block_size = be32toh(sb.blockSize);
+        block_offset = embed_offset / be32toh(sb.blockSize);
+        embed_end = embed_offset + (UInt64)be32toh(hsb.allocationBlockSize) * (UInt16)be16toh(hsb.allocationBlockCount);
 
         // Initialize the bitmap with wrapper blocks (start + end)
         for (i = 0; i < embed_offset / fs_info.block_size; i++)
@@ -314,11 +296,11 @@ void read_super_blocks(char* device, file_system_info* fs_info)
         fs_info->block_size = PART_SECTOR_SIZE;
         fs_info->device_size = partition_size;
         fs_info->totalblock = fs_info->device_size / fs_info->block_size;
-        fs_info->usedblocks = fs_info->totalblock - reverseInt(sb.freeBlocks) * (reverseInt(sb.blockSize) / PART_SECTOR_SIZE);
+        fs_info->usedblocks = fs_info->totalblock - be32toh(sb.freeBlocks) * (be32toh(sb.blockSize) / PART_SECTOR_SIZE);
     } else {
-        fs_info->block_size  = reverseInt(sb.blockSize);
-        fs_info->totalblock  = reverseInt(sb.totalBlocks);
-        fs_info->usedblocks  = reverseInt(sb.totalBlocks) - reverseInt(sb.freeBlocks);
+        fs_info->block_size  = be32toh(sb.blockSize);
+        fs_info->totalblock  = be32toh(sb.totalBlocks);
+        fs_info->usedblocks  = be32toh(sb.totalBlocks) - be32toh(sb.freeBlocks);
         fs_info->device_size = fs_info->block_size * fs_info->totalblock;
     }
     log_mesg(2, 0, 0, 2, "%s: blockSize:%i\n", __FILE__, fs_info->block_size);
